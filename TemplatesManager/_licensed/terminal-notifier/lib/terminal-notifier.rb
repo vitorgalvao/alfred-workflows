c9cf7a76045586264891d39b43a695804924a6c1
Vítor Galvão <info@vitorgalvao.com>
2014-05-29 02:14:26 +0100

    added TemplatesManager

---
+++ TemplatesManager/_licensed/terminal-notifier/lib/terminal-notifier.rb
@@ -0,0 +1,87 @@
+require 'shellwords'
+
+module TerminalNotifier
+  BIN_PATH = File.expand_path('../../vendor/terminal-notifier/terminal-notifier.app/Contents/MacOS/terminal-notifier', __FILE__)
+
+  class UnsupportedPlatformError < StandardError; end
+  # Returns wether or not the current platform is Mac OS X 10.8, or higher.
+  def self.available?
+    @available ||= `uname`.strip == 'Darwin' && `sw_vers -productVersion`.strip >= '10.8'
+  end
+
+  def self.execute(verbose, options)
+    if available?
+      command = [BIN_PATH, *options.map { |k,v| v = v.to_s; ["-#{k}", "#{Shellwords.escape(v[0,1])}#{v[1..-1]}"] }.flatten]
+      command = Shellwords.join(command) if RUBY_VERSION < '1.9'
+      result = ''
+      IO.popen(command) do |stdout|
+        output = stdout.read
+        STDOUT.print output if verbose
+        result << output
+      end
+      result
+    else
+      raise UnsupportedPlatformError, "terminal-notifier is only supported on Mac OS X 10.8, or higher."
+    end
+  end
+
+  # Sends a User Notification and returns whether or not it was a success.
+  #
+  # The available options are `:title`, `:group`, `:activate`, `:open`,
+  # `:execute`, `:sender`, and `:sound`. For a description of each option see:
+  #
+  #   https://github.com/alloy/terminal-notifier/blob/master/README.markdown
+  #
+  # Examples are:
+  #
+  #   TerminalNotifier.notify('Hello World')
+  #   TerminalNotifier.notify('Hello World', :title => 'Ruby')
+  #   TerminalNotifier.notify('Hello World', :group => Process.pid)
+  #   TerminalNotifier.notify('Hello World', :activate => 'com.apple.Safari')
+  #   TerminalNotifier.notify('Hello World', :open => 'http://twitter.com/alloy')
+  #   TerminalNotifier.notify('Hello World', :execute => 'say "OMG"')
+  #   TerminalNotifier.notify('Hello World', :sender => 'com.apple.Safari')
+  #   TerminalNotifier.notify('Hello World', :sound => 'default')
+  #
+  # Raises if not supported on the current platform.
+  def notify(message, options = {}, verbose = false)
+    TerminalNotifier.execute(verbose, options.merge(:message => message))
+    $?.success?
+  end
+  module_function :notify
+
+  # Removes a notification that was previously sent with the specified
+  # ‘group’ ID, if one exists.
+  #
+  # If no ‘group’ ID is given, all notifications are removed.
+  def remove(group = 'ALL', verbose = false)
+    TerminalNotifier.execute(verbose, :remove => group)
+    $?.success?
+  end
+  module_function :remove
+
+  LIST_FIELDS = [:group, :title, :subtitle, :message, :delivered_at].freeze
+
+  # If a ‘group’ ID is given, and a notification for that group exists,
+  # returns a hash with details about the notification.
+  #
+  # If no ‘group’ ID is given, an array of hashes describing all
+  # notifications.
+  #
+  # If no information is available this will return `nil`.
+  def list(group = 'ALL', verbose = false)
+    output = TerminalNotifier.execute(verbose, :list => group)
+    return if output.strip.empty?
+
+    require 'time'
+    notifications = output.split("\n")[1..-1].map do |line|
+      LIST_FIELDS.zip(line.split("\t")).inject({}) do |hash, (key, value)|
+        hash[key] = key == :delivered_at ? Time.parse(value) : (value unless value == '(null)')
+        hash
+      end
+    end
+
+    group == 'ALL' ? notifications : notifications.first
+  end
+  module_function :list
+end
---
+++ TemplatesManager/tmfunctions.rb
@@ -0,0 +1,146 @@
+#!/usr/bin/ruby
+
+require 'fileutils'
+require 'net/http'
+require Dir.getwd + '/_licensed/terminal-notifier/lib/terminal-notifier.rb'
+
+LocalTemplates = Dir.getwd + '/templates/local/'
+RemoteTemplates = Dir.getwd + '/templates/remote'
+
+def finderDir
+  %x{osascript -e 'tell application "System Events"
+     if (name of first process whose frontmost is true) is "Finder" then
+     tell application "Finder" to return (POSIX path of (folder of the front window as alias))
+     else
+     return (POSIX path of (path to home folder))
+     end if
+     end tell'}.gsub(/\n$/, '/')
+end
+
+def notification(message)
+  TerminalNotifier.notify(message, :title => 'TemplatesManager')
+end
+
+def localList
+  Dir.entries(LocalTemplates).reject{ |entry| entry =~ /^(\.{1,2}$|.DS_Store$)/ }
+end
+
+def remoteList
+  File.readlines(RemoteTemplates)
+end
+
+def localEdit
+  system('open', LocalTemplates)
+end
+
+def remoteEdit
+  system('open', '-t', RemoteTemplates)
+end
+
+def localAdd(path)
+  pathBasename = File.basename(path)
+
+  if localList.include?(pathBasename)
+    notification('You already have a template with that name')
+    abort 'A template with that name already exists'
+  else
+    FileUtils.cp_r(path, LocalTemplates)
+    notification('Added to local templates')
+  end
+end
+
+def remoteAdd(url)
+  if remoteList.include?(url)
+    notification('You already have a template with that name')
+    abort 'A template with that name already exists'
+  else
+    File.open(RemoteTemplates, 'a') do |link|
+      link.puts url
+    end
+    notification('Added to remote templates')
+  end
+end
+
+def localDelete(localArrayPos)
+  system(Dir.getwd + '/_licensed/trash/trash', LocalTemplates + localList[localArrayPos])
+end
+
+def remoteDelete(remoteArrayPos)
+  tmpArray = remoteList.delete_at(remoteArrayPos)
+
+  File.open(RemoteTemplates, 'w+') do |line|
+    line.puts tmpArray
+  end
+end
+
+def localInfo
+  puts "<?xml version='1.0'?><items>"
+
+  if localList.empty?
+    puts "<item uuid='none' arg='none' valid='no'>"
+    puts "<title>List templates (tml)</title>"
+    puts "<subtitle>You need to add some local templates, first</subtitle>"
+    puts "<icon>icon.png</icon>"
+    puts "</item>"
+  else
+    localList.each_index do |localArrayPos|
+      templateTitle = localList[localArrayPos]
+
+      puts "<item uuid='#{localArrayPos}' arg='#{localArrayPos}' valid='yes'>"
+      puts "<title><![CDATA[#{templateTitle}]]></title>"
+      puts "<icon>icon.png</icon>"
+      puts "</item>"
+    end
+  end
+
+  puts "</items>"
+end
+
+def remoteInfo
+  puts "<?xml version='1.0'?><items>"
+
+  if remoteList.empty?
+    puts "<item uuid='none' arg='none' valid='no'>"
+    puts "<title>List templates (rtml)</title>"
+    puts "<subtitle>You need to add some remote templates, first</subtitle>"
+    puts "<icon>icon.png</icon>"
+    puts "</item>"
+  else
+    remoteList.each_index do |remoteArrayPos|
+      templateSubtitle = remoteList[remoteArrayPos]
+      templateTitle = File.basename(templateSubtitle)
+
+      puts "<item uuid='#{remoteArrayPos}' arg='#{remoteArrayPos}' valid='yes'>"
+      puts "<title><![CDATA[#{templateTitle}]]></title>"
+      puts "<subtitle><![CDATA[#{templateSubtitle}]]></subtitle>"
+      puts "<icon>icon.png</icon>"
+      puts "</item>"
+    end
+  end
+
+  puts "</items>"
+end
+
+# Copy files and directories directly
+def localPut(localArrayPos)
+  FileUtils.cp_r(LocalTemplates + localList[localArrayPos], finderDir)
+end
+
+# If source is a directory, copy what's inside of it
+def localPutFilesOnly(localArrayPos)
+  itemLocation = LocalTemplates + localList[localArrayPos]
+
+  # if used on a file, give a warning
+  if File.file?(itemLocation)
+    notification('This option should only be used on directories')
+    abort 'This option should only be used on directories'
+  else
+    FileUtils.cp_r(Dir[itemLocation + '/*'], finderDir)
+  end
+end
+
+def remotePut(remoteArrayPos)
+  url = remoteList[remoteArrayPos].gsub(/\n$/, '')
+  fileName = File.basename(url)
+  File.write(finderDir + fileName, Net::HTTP.get(URI.parse(url)))
+end
