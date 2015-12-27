013b8010bdf508446c002440a511e98b4eaead14
Vítor Galvão <info@vitorgalvao.com>
2015-12-27 02:51:35 +0000

    support for downloading playlists

---
+++ DownVid/downvid_restart.sh
@@ -0,0 +1,16 @@
+# set notifications
+notification() {
+  ./_licensed/terminal-notifier/terminal-notifier.app/Contents/MacOS/terminal-notifier -title 'DownVid' -message "$1"
+}
+
+if [[ "$1" == 'restart_download_true' ]]; then
+  notification 'Restarting download at bottom of queue'
+  currentquery=$(cat '/tmp/downvidcurrentquery')
+
+  osascript -e "tell application \"Alfred 2\" to run trigger \"new\" in workflow \"com.vitorgalvao.alfred.downvid\" with argument \"${currentquery}\""
+else
+  notification 'Download aborted'
+  rm '/tmp/downvidprogress'
+fi
+
+kill $(ps -A | grep 'downvid.sh' | awk '{print $1}')
