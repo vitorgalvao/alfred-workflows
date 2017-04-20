eb56a89539f0b3ad7f6fdf13239e8526b9b5d429
Vítor Galvão <info@vitorgalvao.com>
2017-04-20 12:01:48 +0100

    BugNot: New icon

--- BugNot/source/bugnot.rb
+++ BugNot/source/bugnot.rb
@@ -10,7 +10,7 @@ abort 'You need to specify a website' if query.nil?
 script_filter_items = []
 
 begin
-  page = Nokogiri::HTML(open('http://bugmenot.com/view/' + query))
+  page = Nokogiri::HTML(open("http://bugmenot.com/view/#{query}"))
 rescue OpenURI::HTTPError
   script_filter_items.push(title: "No logins availale for “#{query}”", valid: 'no')
 else
@@ -20,7 +20,7 @@ else
     login_section = page.css('article.account')[account]
     username = login_section.css('kbd')[0].text
     password = login_section.css('kbd')[1].text
-    success = login_section.css('li.success_rate').attr('class').text.gsub(/\D*/, '')
+    success = login_section.css('li.success_rate').attr('class').text.sub(/\D*/, '')
 
     script_filter_items.push(title: username.encode(xml: :text), subtitle: password.encode(xml: :text), icon: { path: "icons/#{success}.png" }, arg: "#{username}⸗#{password}")
   end
