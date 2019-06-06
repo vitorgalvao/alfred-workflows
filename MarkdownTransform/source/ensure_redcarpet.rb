a5ec5d4a7a330a9b0c227ca069c32d7f3ba26208
Daniel <daniel@sdunixgeek.com>
2019-06-06 12:26:28 -0700

    Adding code block support using Rouge
    * Added rouge to the html and rtf script so that syntax code highlighting could be supported for both exports.

--- MarkdownTransform/source/ensure_redcarpet.rb
+++ MarkdownTransform/source/ensure_redcarpet.rb
@@ -2,20 +2,22 @@
 
 Redcarpet_dir = "#{__dir__}/_licensed/Redcarpet".freeze
 $LOAD_PATH.unshift "#{Redcarpet_dir}/gems/redcarpet-3.4.0/lib"
-
+$LOAD_PATH.unshift "#{Redcarpet_dir}/gems/rouge-3.3.0/lib"
 def notification(subtitle, message, sound = '')
   system("#{__dir__}/Notificator.app/Contents/Resources/Scripts/notificator", '--message', message, '--subtitle', subtitle, '--sound', sound, '--title', ENV['alfred_workflow_name'])
 end
 
 begin
   require 'redcarpet'
+  require 'rouge'
+  require 'rouge/plugins/redcarpet'
 rescue LoadError
-  notification('Reinstalling converter…', 'Please wait a few seconds')
+  notification('Reinstalling converter and syntax highlighter', 'Please wait a few seconds')
 
   require 'fileutils'
   FileUtils.rm_r(Redcarpet_dir) if Dir.exist?(Redcarpet_dir)
 
-  if system('/usr/bin/gem', 'install', '--no-document', '--install-dir', Redcarpet_dir, 'redcarpet', out: File::NULL)
+  if system('/usr/bin/gem', 'install', '--no-document', '--install-dir', Redcarpet_dir, 'redcarpet', 'rouge', out: File::NULL)
     notification('Finished reinstalling!', 'This should not be necessary again', 'Ping')
   else
     notification('There was an error', 'Please open a bug report', 'Funk')
