526d4a7c7930a603869ac1a92265430213911ff0
Vítor Galvão <info@vitorgalvao.com>
2020-04-04 02:05:30 +0100

    WatchList: Do not close Alfred on play

--- WatchList/source/watchlist_functions.rb
+++ WatchList/source/watchlist_functions.rb
@@ -458,7 +458,7 @@ def trash(path)
 end
 
 def notification(message, sound = '')
-  system("#{__dir__}/Notificator.app/Contents/Resources/Scripts/notificator", '--message', message, '--title', ENV['alfred_workflow_name'], '--sound', sound)
+  system("#{Dir.pwd}/Notificator.app/Contents/Resources/Scripts/notificator", '--message', message, '--title', ENV['alfred_workflow_name'], '--sound', sound)
 end
 
 def error(message)
