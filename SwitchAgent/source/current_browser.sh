3a54f160e7d36431e0beed3716d6e8c4b67b195a
Vítor Galvão <info@vitorgalvao.com>
2021-05-24 02:35:01 +0100

    SwitchAgent: Write to cache dir instead of data dir

--- SwitchAgent/source/current_browser.sh
+++ SwitchAgent/source/current_browser.sh
@@ -15,7 +15,12 @@ current_browser="$(osascript -l JavaScript -e '
     }
 ')"
 
-[[ "${current_browser}" == 'Unsupported' ]] && error 'You need a supported web browser as your frontmost app.'
-[[ "${current_browser}" == 'Safari' ]] && [[ "$(defaults read com.apple.Safari IncludeDevelopMenu)" -eq 1 ]] || [[ "${current_browser}" == 'Safari Technology Preview' ]] && [[ "$(defaults read com.apple.SafariTechnologyPreview WebKitDeveloperExtrasEnabledPreferenceKey)" -eq 1 ]] && error 'You need to enable the Develop menu.'
+if [[ "${current_browser}" == 'Safari' ]]; then
+  [[ "$(defaults read com.apple.Safari IncludeDevelopMenu)" -eq 0 ]] && error 'You need to enable the Develop menu.'
+elif [[ "${current_browser}" == 'Safari Technology Preview' ]]; then
+  [[ "$(defaults read com.apple.SafariTechnologyPreview WebKitDeveloperExtrasEnabledPreferenceKey)" -eq 0 ]] && error 'You need to enable the Develop menu.'
+else
+  error 'You need a supported web browser as your frontmost app.'
+fi
 
 echo -n "${current_browser}" # Will only output if the previous checks pass
