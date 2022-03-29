5c8239e9fbc2c520b89cd31856275997289c6697
Vítor Galvão <info@vitorgalvao.com>
2022-03-29 17:40:27 +0100

    PinPlus: Update notificator and OneUpdater

--- PinPlus/source/pinplus.rb
+++ PinPlus/source/pinplus.rb
@@ -9,7 +9,7 @@ All_bookmarks_json = "#{ENV['alfred_workflow_cache']}/all_bookmarks.json".freeze
 Unread_bookmarks_json = "#{ENV['alfred_workflow_cache']}/unread_bookmarks.json".freeze
 
 def notification(message, title = ENV['alfred_workflow_name'])
-  system('/bin/zsh', "#{Dir.pwd}/Notificator.app/Contents/Resources/Scripts/notificator", '--message', message, '--title', title)
+  system("#{Dir.pwd}/notificator", '--message', message, '--title', title)
 end
 
 def success_sound
