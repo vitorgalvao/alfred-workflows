767d567b1f9d75d988e854c6544a85a142a86a1f
Dorian Karter <dkarter@users.noreply.github.com>
2022-12-17 22:08:11 -0600

    feat: add Firefox support

--- DownMedia/source/get_title_and_url.js
+++ DownMedia/source/get_title_and_url.js
@@ -1,17 +1,23 @@
 #!/usr/bin/osascript -l JavaScript
 
-const frontmost_app_name = Application('System Events').applicationProcesses.where({ frontmost: true }).name()[0]
-const frontmost_app = Application(frontmost_app_name)
+const frontmost_app = Application('System Events').applicationProcesses.where({ frontmost: true })
+const frontmost_app_name = frontmost_app.name()[0]
 
 const chromium_variants = ['Google Chrome', 'Chromium', 'Opera', 'Vivaldi', 'Brave Browser', 'Microsoft Edge']
 const webkit_variants = ['Safari', 'Webkit']
+const firefox_variants = ['firefox']
+
+let current_tab_title, current_tab_url;
 
 if (chromium_variants.some(app_name => frontmost_app_name.startsWith(app_name))) {
-  var current_tab_title = frontmost_app.windows[0].activeTab.name()
-  var current_tab_url = frontmost_app.windows[0].activeTab.url()
+  current_tab_title = frontmost_app.windows[0].activeTab.name()
+  current_tab_url = frontmost_app.windows[0].activeTab.url()
 } else if (webkit_variants.some(app_name => frontmost_app_name.startsWith(app_name))) {
-  var current_tab_title = frontmost_app.windows[0].currentTab.name()
-  var current_tab_url = frontmost_app.windows[0].currentTab.url()
+  current_tab_title = frontmost_app.windows[0].currentTab.name()
+  current_tab_url = frontmost_app.windows[0].currentTab.url()
+} else if (firefox_variants.some(app_name => frontmost_app_name.startsWith(app_name))) {
+  current_tab_title = frontmost_app.windows[0].title()
+  current_tab_url = frontmost_app.windows[0].groups[0].toolbars.byName('Navigation').comboBoxes[0].uiElements[0].value()
 } else {
   throw new Error('You need a supported browser as your frontmost app')
 }
